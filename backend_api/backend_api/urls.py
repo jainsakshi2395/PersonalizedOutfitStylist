a3a25d415100f86be23df198127ef96c614572e5
simran-memon <simranmemon131@gmail.com>
2023-04-03 00:42:44 -0700

    api to recommend outfits based on users - age, bodytype, season selected

--- backend_api/backend_api/urls.py
+++ backend_api/backend_api/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('image_file/', include('backend_app_1.urls')),
+    path('', include('backend_app_1.urls'))
 ]
 
 if settings.DEBUG:
--- backend_api/backend_app_1/views.py
+++ backend_api/backend_app_1/views.py
@@ -1,15 +1,12 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import JsonResponse
 from .serializers import UserImageSerializer, OutfitSerializer
-from django.http import FileResponse, HttpResponse
-from rest_framework import viewsets
+from .apps import BackendApp1Config
+from django.http import HttpResponse, JsonResponse
 from .models import Outfit, UserImage
-import zipfile
-from io import BytesIO
-# Create your views here.
 
 
 class ImageUploadView(APIView):
@@ -19,29 +16,44 @@ class ImageUploadView(APIView):
         user_image_serializer = UserImageSerializer(data=request.data)
         if user_image_serializer.is_valid():
             user_image_serializer.save()
-            # img_path = File.objects.get(id=2)
-            # img = open(img_path, 'rb')
-            # img = open(img_path, 'rb')
-            # img2 = open('media/bootstrap.jpeg', 'rb')
-            # img_res = FileSerializer(img_path)
-            # response = FileResponse(img2)
-            # return HttpResponse(img_res, content_type="image/png")
-        #image_details = Outfit.objects.get(pk=image_id)
 
         image_details = Outfit.objects.filter(pk__in=[59679, 59680, 59681])
         serializer = OutfitSerializer(image_details, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-            # outfile = BytesIO()
-            # with zipfile.ZipFile(outfile, 'w') as zf:
-            #     for item in rc_files:
-            #         zf.writestr(f"{item.remark}.png", item.file.read())
-            #
-            # response = HttpResponse(outfile.getvalue(), content_type='application/zip')
-            # response['Content-Disposition'] = 'attachment; filename=my_file.zip'
-            # return response
+class RecommendAll(APIView):
+    def post(self, request, *args, **kwargs):
 
-            # return Response(file_serializer.data, status=status.HTTP_201_CREATED)
-        # else:
-        #     return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        age_group = request.data.get('age_group')  # accepting as string = "Teen"  || "Children" || "Adult"
+        body_type = request.data.get('body_type')   # accepting as string = "Pear" || "Rectangle" || ..
+        season = request.data.get('season')         # accepting as string = "Fall" || "Winter" || ..
+        recommended_response = list()
+
+
+        if age_group:
+            # write the code to call Model 1
+            # It should have response as list of dictionary
+
+            # adding sample output to test for now
+            image_details = Outfit.objects.filter(pk__in=[59679, 59680, 59681])
+            serializer = OutfitSerializer(image_details, many=True)
+            recommended_response += serializer.data
+
+        if body_type:
+            # write the code to call model 2
+            # It should have response as list of dictionary
+            pass
+
+        if season:
+            # write the code to call model 3
+            # It should have response as list of dictionary
+            pass
+
+        return Response(recommended_response, status=status.HTTP_200_OK)
+
+
+class Default(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {"This is API server, Use Postman"}
+        return Response(data, status=status.HTTP_200_OK)
