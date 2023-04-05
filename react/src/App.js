87cab8f24b10dbd029ada66f12d396a4b7f35fba
Supreetha M A <supreethama@gmail.com>
2023-04-05 13:14:45 -0700

    Integration of Profile-create api and addition of redux infrastructure

--- react/src/App.js
+++ react/src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Register from './pages/Register';
 import Upload from './pages/Upload';
+import { Provider } from 'react-redux';
+import './App.css';
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-      <Navbar />
-        <Routes>
-        {/* @TODO - change to the right components when other components are ready. */}
-          <Route exact path='/' element={<Upload />}/>
-          <Route exact path='/home' element={<Register />}/>
-          <Route exact path='/register' element={<Register />} />
-          <Route exact path='/upload' element={<Upload />} />
-        </Routes>
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Router>
+        <Navbar />
+          <Routes>
+          {/* @TODO - change to the right components when other components are ready. */}
+            <Route exact path='/' element={<Upload />}/>
+            <Route exact path='/home' element={<Register />}/>
+            <Route exact path='/register' element={<Register />} />
+            <Route exact path='/upload' element={<Upload />} />
+          </Routes>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
