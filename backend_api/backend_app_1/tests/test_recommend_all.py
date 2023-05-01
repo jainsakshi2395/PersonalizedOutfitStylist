fc0fe78144efbe20f9dda5067ed3d5a0856825ba
simran-memon <simranmemon131@gmail.com>
2023-05-01 00:51:17 -0700

    Test cases for recommendations based on age, bodytype, season api's

--- backend_api/backend_app_1/tests/test_recommend_all.py
+++ backend_api/backend_app_1/tests/test_recommend_all.py
@@ -1,10 +1,11 @@
-# import pytest
 from rest_framework import status
 from rest_framework.test import APITestCase
 from django.urls import reverse
 
 
 class TestRecommendAll(APITestCase):
+
+    # checking for user's valid physical attributes
     def test_recommend_user_attr_200(self):
         url = reverse('recommend_all')
         data = {'user_age': 14, 'user_hip': 7, 'user_bust': 7, 'user_waist': 3}
@@ -12,6 +13,7 @@ class TestRecommendAll(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         print("Recommendations for user attributes - valid inputs")
 
+    # checking for user's invalid physical attributes
     def test_recommend_user_attr_400(self):
         url = reverse('recommend_all')
         data = {'user_age': "fourteen", 'user_hip': 7, 'user_bust': 5, 'user_waist': 3}
@@ -19,6 +21,7 @@ class TestRecommendAll(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         print("Recommendations for user attributes - invalid inputs")
 
+    # checking for user's valid age
     def test_recommend_user_attr_age_200(self):
         url = reverse('recommend_all')
         data = {'user_age': 5, 'user_hip': 7, 'user_bust': 7, 'user_waist': 3}
@@ -26,7 +29,7 @@ class TestRecommendAll(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         print("Recommendations for user attributes - user's age valid check")
 
-
+    # checking for user's invalid age
     def test_recommend_user_attr_age_400(self):
         url = reverse('recommend_all')
         data = {'user_age': "three", 'user_hip': 7, 'user_bust': 7, 'user_waist': 3}
@@ -34,21 +37,23 @@ class TestRecommendAll(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         print("Recommendations for user attributes - user's age invalid check")
 
+    # checking for user's valid bodytype
     def test_recommend_user_attr_bodytype_200(self):
         url = reverse('recommend_all')
         data = {'user_age': 14, 'user_hip': 7, 'user_bust': 7, 'user_waist': 3}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        print("Recommendations for user attributes - user's physical parameters valid check")
+        print("Recommendations for user attributes - user's bodytype valid check")
 
+    # checking for user's invalid bodytype
     def test_recommend_user_attr_bodytype_400(self):
         url = reverse('recommend_all')
         data = {'user_age': 17, 'user_hip': 7, 'user_bust': 0, 'user_waist': 3}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        print("Recommendations for user attributes - user's physical parameters invalid check")
+        print("Recommendations for user attributes - user's bodytype invalid check")
 
-
+    # checking for valid selected filters
     def test_recommend_selected_filters_200(self):
         url = reverse('recommend_all')
         data = {'age_group': "Teen", 'body_type': 'Rectangle', 'season': 'winter'}
@@ -56,6 +61,7 @@ class TestRecommendAll(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         print("Recommendations for selected filters - valid check")
 
+    # checking for invalid selected filters
     def test_recommend_selected_filters_400(self):
         url = reverse('recommend_all')
         data = {'age_group': "Teen", 'body_type': 123, 'season': 'winter'}
