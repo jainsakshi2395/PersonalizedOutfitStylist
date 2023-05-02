36f4bd2f3299bc32252ed4d2873071200a4ad0cc
shivanipandit88 <shivani.pandit@sjsu.edu>
2023-05-02 14:57:40 -0700

    resolving conflicts in frontend

--- react/src/App.js
+++ react/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Register from './pages/Register';
-import Upload from './pages/Upload';
 import Recommend from './pages/Recommend';
 import Details from './pages/Details';
 import NewUser from './pages/NewUser';
@@ -18,7 +17,7 @@ function App() {
         <Navbar />
           <Routes>
           {/* @TODO - change to the right components when other components are ready. */}
-            <Route exact path='/' element={<Upload />}/>
+            <Route exact path='/' element={<NewUser />}/>
             <Route exact path='/home' element={<Register />}/>
             <Route exact path='/register' element={<Register />} />
             <Route exact path='/recommend' element={<Recommend />} />
