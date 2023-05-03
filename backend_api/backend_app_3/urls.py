15563662541b9eac7c978a4f9a56d2b09f33a81f
Sakshi Jain <sakshi.jain01@sjsu.edu>
2023-05-03 14:45:35 -0700

    user profile Apis model changed

---
+++ backend_api/backend_app_3/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('profile/create', views.createMeasurements, name='measurement-create'),
+    path('profile/get/<uuid:user_id>', views.getMeasurementByUserId, name='get-measurement-by-id'),
+    path('profile/update/<uuid:user_id>', views.updateMeasurementByUserId, name='update-measurement-by-id'),
+]
