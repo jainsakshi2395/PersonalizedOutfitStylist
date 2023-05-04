fb91dca65ef277d78416242fe9a4837bbda3a238
Supreetha M A <supreethama@gmail.com>
2023-05-04 15:11:10 -0700

    Revert "Redux for Filter API integration"
    This reverts commit 59c90e8b34d51cb93309b665ef761d7b0fadc73f.

--- react/src/rootReducer.js
+++ react/src/rootReducer.js
@@ -6,7 +6,6 @@ import setPreviewReducer from './redux/updatePreview/setPreviewReducer';
 import cardDetailsReducer from './redux/cardDetails/cardDetailsReducer';
 import profileDetailsReducer from './redux/profileDetails/profileDetailsReducer';
 import updateProfileReducer from './redux/updateProfile/updateProfileReducer';
-import filterReducer from './redux/filter/filterReducer';
 
 const rootReducer = combineReducers({
   profile: profileReducer,
@@ -16,7 +15,6 @@ const rootReducer = combineReducers({
   cardDetails: cardDetailsReducer,
   profileDetails: profileDetailsReducer,
   updateProfile: updateProfileReducer,
-  filter: filterReducer,
 });
 
 export default rootReducer;
